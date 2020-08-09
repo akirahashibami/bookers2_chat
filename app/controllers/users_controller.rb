400f1118f172617494a61ca73d1e40eb7cf0e7b2
vagrant <hashibami2207@gmail.com>
2020-08-09 06:26:36 +0000

    [Add] view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    # チャット機能の定義します
+      if user_signed_in?
+        @currentUserEntry = UserRoom.where(user_id: current_user.id)
+        @userEntry = UserRoom.where(user_id: @user.id)
+        unless @user.id == current_user.id
+          @currentUserEntry.each do |cu|
+            @userEntry.each do |u|
+              if cu.room_id == u.room_id
+                @haveRoom = true
+                @roomId = cu.room_id
+              end
+            end
+          end
+          unless @haveRoom
+            @room = Room.new
+            @user_room = UserRoom.new
+          end
+        end
+      end 
   end
 
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
+  has_many :chats
+  has_many :user_rooms
+  has_many :rooms, through: :user_rooms
+
 
   # ひょろわ〜機能
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
